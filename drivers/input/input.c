cc13c29b9493bb47df97f7afd2ab28ef668f7398
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2023-12-26 13:18:10 +0100

    Enable KernelSU safe mode

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -216,10 +216,16 @@ static int input_handle_abs_event(struct input_dev *dev,
 	return INPUT_PASS_TO_HANDLERS;
 }
 
+extern bool ksu_input_hook __read_mostly;
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition = INPUT_IGNORE_EVENT;
+
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
 
 	switch (type) {
 
